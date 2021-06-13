00cdf3d8aed841fd86e3d3df2bda0cf54bddcfae
mariusz23343 <mariusz.byczek99@gmail.com>
2021-06-13 17:26:49 +0200

    extracting the mapper method into a new class

---
+++ MeetingApi/MeetsMapper.cs
@@ -0,0 +1,42 @@
+﻿using MeetingApi.Models.DbModels;
+using MeetingApi.Models.DbModels.DTOs;
+using MeetingApi.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetingApi
+{
+    public static  class MeetsMapper
+    {
+        public static List<MeetDTO> MapResponse(List<Meet> newMeetList)
+        {
+            var meetsToReturn = new List<MeetDTO>();
+            foreach (var item in newMeetList)
+            {
+                meetsToReturn.Add(new MeetDTO
+                {
+                    MeetDate = item.MeetDate,
+                    MeetName = item.MeetName
+                });
+            }
+            return meetsToReturn;
+        }
+        public static List<ParticipantDTO> MapResponse(List<Participant> participants)
+        {
+            var participantsToReturn = new List<ParticipantDTO>();
+            foreach (var item in participants)
+            {
+                participantsToReturn.Add(new ParticipantDTO
+                {
+                    Email = item.Email,
+                    ParticipantName = item.ParticipantName,
+                    MeetId = item.FkMeet
+                });
+            }
+
+            return participantsToReturn;
+        }
+    }
+}
